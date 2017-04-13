66a3c869501fa2d7e1222e5c3a42b6fce0340c12
Dennis Reimer <dennisreimer90@googlemail.com>
2017-04-13 15:15:26 +0200

    Testscene and Movement with Keyboard

---
+++ Assets/TopDownWalkingTutorial/Scripts/Movement.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float MoveSpeed;
+
+    private Rigidbody myRigidbody;
+    private Vector3 moveVelocity;
+
+    // Use this for initialization
+    void Start()
+    {
+        myRigidbody = this.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        var moveDirection = new Vector3(horizontal, 0f, vertical);
+
+        moveVelocity = moveDirection * MoveSpeed;
+
+        this.transform.LookAt(this.transform.position + moveDirection);
+    }
+
+    void FixedUpdate()
+    {
+        myRigidbody.velocity = moveVelocity;
+    }
+}
